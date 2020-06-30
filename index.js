f05baaf343f73c615485f558233e6c0b9832d3a0
김준호 <enddl0121@gmail.com>
2020-06-30 19:44:29 +0900

    삭제 다시해야함

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 
 const config = require("./config/key")
 
+const { auth } = require("./middleware/auth")
 const { User } = require("./models/User");
 
 
@@ -29,6 +31,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 //json 타입으로 된것을 분석해서 가져온다.
 app.use(bodyParser.json());
 
+//express에서 제공되는 모듈 -> app.
+app.use(cookieParser());
+
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -38,20 +43,95 @@ mongoose.connect(config.mongoURI,{
 
 app.get('/',(req,res)=> res.send("Hi"))
 
-app.post('/register',(req,res)=> {
+app.post('/api/users/register',(req,res)=> {
   //회원가입할떄 필요한 정보들을 client에서 가져오면
   //그것을들 데이터 베이스에 넣어준다.
 
   //req.body 안에는 json 형식으로 들어있다.
     const user = new User(req.body)
+
   //save는 몽고db에서 오는 메소드
     user.save((err, userInfo)=>{
       if(err) return res.json({ success: false, err})
       return res.status(200).json({
         success: true
+    })
+  })
+})
+
+
+app.post('/api/users/login', (req, res)=> {
+  //요청된 이메일을 데이터베이스에 있는지 찾는다. //findeOne => 몽고디비에서 지원하는 메소드
+  User.findOne({ email: req.body.email }, (err, user)=>{
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+  //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+  user.comparePassword(req.body.password, (err, isMatch) => {
+     if(!isMatch)
+     return res.json({loginSuccess: false, message: "비밀번호가 틀렷습니다."
+    })   
+  //비밀 번호까지 같다면 Token 생성
+    user.generateToken((err, user)=> {
+      if(err) return res.status(400).send(err);  //status 400 => 실패했다는 뜻 -> send(err)
+                     
+      // 토큰을 저장한다. 어디에?  쿠키, 로컬스토리지... 저장하는 방법은 여러가지가 있다.
+      // 쿠키에 저장하기 위해서는 모듈을 설치해야한다. 
+          res.cookie("x_auth", user.token)
+          .status(200) //성공했다는 표시
+          .json({ loginSuccess: true, userId: user._id })
       })
     })
+  })
 })
+
+// role 0 -> 일반유저 role이 0이 아니면 관리자
+//ex) role 1 -> 어드민 , role 2 => 특정 부서 어드민
+
+                  //auth는 미드웨어 콜백함수를 하기전에 실행된다.
+app.get('/api/users/auth', auth, (req, res)=>{
+
+  //여기 까지 미들웨어를 통과해 왔다는 이야기는 Auth가 ture라는 말.
+  res.status(200).json({
+   _id: req.user._id,
+   isAdmin: req.user.role === 0 ? false : true,
+   isAuth: true,
+   email: req.user.email,
+   name: req.user.name,
+   lastname: req.user.lastname,
+   role: req.user.role,
+   image: req.user.image
+  })
+
+})
+
+//로그아웃은 로그인된 상태이기 떄문에 auth 미들웨어를 사용해야한다.
+app.get('/api/users/logout', auth, (req, res) => {
+  console.log('req.user', req.user)
+  User.findByIdAndDelete({_id: req.user._id},
+    {token:""} //토큰을 지워줌
+   , (err, user) => {
+     //에러가 생겼을떄
+     if(err) return res.json({ success :false, err});
+     //성공
+     return res.status(200).send({
+       success: true
+     })
+   })
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
